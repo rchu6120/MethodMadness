b1bd1c63b74b09c4000295fce0624755d27ae299
rchu9700 <rchu9700@bths.edu>
2018-10-02 14:50:42 -0400

    1st one done

--- src/RChuLib.java
+++ src/RChuLib.java
@@ -1,5 +1,10 @@
 public class RChuLib {
 }
-    public static boolean isPalindrome(String str1) {
-      for (int i=0; i < str1.length(); i++)
-    }
+    public static boolean isPalindrome(String word) {
+            for (int i = 0; i < word.length()/2; i++) {
+                if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
--- src/testFile.java
+++ src/testFile.java
@@ -1,5 +1,10 @@
 public class testFile {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("Hello Method Madness");
+        System.out.println();
     }
+    public static void main (String[] args) {RChuLib.isPalindrome("racecar");}
 }
+
+
