794109f1007833db292c93d6659c2553c56613d6
rchu9700 <rchu9700@bths.edu>
2018-10-04 15:33:22 -0400

    3 codes down

--- src/RChuLib.java
+++ src/RChuLib.java
@@ -9,5 +9,27 @@ public class RChuLib {
         return true;
     }
 
-    public static
+    public static String dateStr(String date) {
+        String month = date.substring(0,2);
+        String day = date.substring(3,5);
+        String year = date.substring(6,10);
+        return day + "-" + month + "-" + year;
+    }
+
+    /*
+    public static String cutOut(String mainStr,String subStr) {
+      for (int i=0; i <= mainStr.length()-1; i++) {
+          if (subStr)
+      }
+    }
+*/
+    public static void printFibonnaci (int n) {
+        int num1 = 0;
+        int num2 = 1;
+        while (num1 + num2 < n) {
+            System.out.println(num1+num2);
+            num1 = num2;
+            num2 = num1 + num2;
+        }
+    }
 }
--- src/testFile.java
+++ src/testFile.java
@@ -3,6 +3,8 @@ public class testFile {
     public static void main(String[] args) {
         System.out.println("Hello Method Madness");
         System.out.println(RChuLib.isPalindrome("racecar"));
+        System.out.println(RChuLib.dateStr("06/12/2001"));
+        RChuLib.printFibonnaci(37);
     }
 
 }
